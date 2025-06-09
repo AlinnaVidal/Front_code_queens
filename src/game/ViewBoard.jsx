0b83915b427b190543163a4c9a483a245dd579db
FLORENCIA SCHIAPPACASSE <fschiappacasse@uc.cl>
2025-06-08 20:50:58 -0400

    fix al merge

--- src/game/ViewBoard.jsx
+++ src/game/ViewBoard.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState, lazy } from 'react';
+import {useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import { AuthContext } from '../auth/AuthContext';
+
 
 import blueB from '../assets/tablero/bloque_azul.png'
 import orangeB from '../assets/tablero/bloque_naranja.png'
@@ -40,15 +43,25 @@ function setCellColor(color){
 
 
 
-function Board(gameId){
+function Board({gameId}){
     const [board, setBoard] = useState([]);
+    const { token } = useContext(AuthContext);
     
     useEffect(() => {
-    fetch(`${import.meta.env.VITE_BACKEND_URL}/mechanics/view/${gameId}`)
-        .then(res => res.json())
-        .then(data => setBoard(data['board']))
-        .catch(err => console.error('Error al obtener partidas:', err));
-    }, []);
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/mechanics/view/${gameId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+    })
+      .then(res => {
+        if (!res.ok) throw new Error(`Error ${res.status}`);
+        return res.json();
+      })
+      .then(data => setBoard(data.board))
+      .catch(err => console.error('Error al obtener el tablero:', err));
+  }, [gameId]);
 
     return (
         <div className="board">
@@ -120,7 +133,7 @@ function ViewBoard(){
 
     return (
         <div>
-            {Board(gameId)}
+        <Board gameId={gameId} />
         </div>
     )
 }
